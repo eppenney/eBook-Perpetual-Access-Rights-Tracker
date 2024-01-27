790f0a337b48b67519246ce288ab62d5d6d8cd21
iewolters <iewolters@upei.ca>
2024-01-26 20:35:13 -0400

    Scraping file added - includes functions for scraping CRKN website and one that is applicable to local files config.py includes settings database.py includes setting up database and tables

---
+++ src/data_processing/Scraping.py
@@ -0,0 +1,108 @@
+"""
+Isaac Wolters
+January 26, 2024,
+
+This file includes functions for scraping from the CRKN website and uploading the new data to the database
+Some functions can also be re-used for the local file uploads (compare_file)
+
+Works well I think - should test the update functionality
+I tested new files and the same files, but not when the file has a newer date (to update)
+
+In the compare_file function, I have repeating code - if someone knows a good way to fix that, go for it
+"""
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from src.utility import config
+import database
+
+
+def scrapeCRKN():
+	page_text = requests.get(config.CRKN_url).text
+	soup = BeautifulSoup(page_text, "html.parser")
+
+	links = soup.find_all('a', href=lambda href: href and (href.endswith('.xlsx')))
+	# Maybe include some other options in here? CSV, other excel format?
+
+	connection = database.connect_to_database()
+	for link in links:
+		file_link = link.get("href")
+		file_first, file_date = split_CRKN_file_name(file_link)
+		result = compare_file([file_first, file_date], "CRKN", connection)
+
+		# If file does not exist/is not up-to-date
+		if not result:
+			with open("temp.xlsx", 'wb') as file:
+				response = requests.get(config.CRKN_root_url + file_link)
+				file.write(response.content)
+			upload_to_database("temp.xlsx", file_first, connection)
+	database.close_database(connection)
+
+
+def compare_file(file, method, connection):
+	# Repeating code, not sure the best way to resolve it - does it work to replace it with the method variable?
+
+	# True means already exists
+	# False means does not exist
+	if method != "CRKN" and method != "local":
+		raise Exception("Incorrect method type (CRKN or local) to indicate type/location of file")
+
+	if method == "CRKN":
+		cursor = connection.cursor()
+		files = cursor.execute(f"SELECT * FROM CRKN_file_names WHERE file_name = '{file[0]}'").fetchall()
+		if not files:
+			cursor.execute(f"INSERT INTO CRKN_file_names (file_name, file_date) VALUES ('{file[0]}', '{file[1]}')")
+			print(f"file name inserted - {file[0]}, {file[1]}")
+			return False
+		else:
+			files_dates = cursor.execute(f"SELECT * FROM CRKN_file_names WHERE file_name = '{file[0]}' and file_date = '{file[1]}'").fetchall()
+			if not files_dates:
+				cursor.execute(f"UPDATE CRKN_file_names SET file_date = '{file[1]}' WHERE file_name = '{file[0]}';")
+				print(f"file name updated - {file[0]}, {file[1]}")
+				return False
+			print(f"File already there - {file[0]}, {file[1]}")
+			return True
+
+	else:   # local method
+		cursor = connection.cursor()
+		files = cursor.execute(f"SELECT * FROM Local_file_names WHERE file_name = '{file[0]}'").fetchall()
+		if not files:
+			cursor.execute(f"INSERT INTO Local_file_names (file_name, file_date) VALUES ('{file[0]}', '{file[1]}')")
+			print(f"file name inserted - {file[0]}, {file[1]}")
+			return False
+		else:
+			files_dates = cursor.execute(f"SELECT * FROM Local_file_names WHERE file_name = '{file[0]}' and file_date = '{file[1]}'").fetchall()
+			if not files_dates:
+				cursor.execute(f"UPDATE Local_file_names SET file_date = '{file[1]}' WHERE file_name = '{file[0]}';")
+				print(f"file name updated - {file[0]}, {file[1]}")
+				return False
+			print(f"File already there - {file[0]}, {file[1]}")
+			return True
+
+
+def split_CRKN_file_name(file_name):
+	# Split the date part of the file name from the first half
+	file = file_name.split("/")[-1]
+	a = file.split("_")
+	c = "_".join(a[3:]).split(".")[0]
+	return [a[2], c]
+
+
+def upload_to_database(file, table_name, connection):
+	# File can be either a file or a URL link to a file
+	try:
+		df = pd.read_excel(file, sheet_name="PA-Rights")
+	except ValueError:
+		df = pd.read_excel(file, sheet_name="PA-rights")
+
+	df.to_sql(
+		name=table_name,
+		con=connection,
+		if_exists="replace",
+		index=False
+	)
+
+
+scrapeCRKN()
