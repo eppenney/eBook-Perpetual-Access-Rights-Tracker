17ebf6bc1454370948ddee1aa8dc4d38dd801d81
crbottomley-mason <crbottomley-mason@upei.ca>
2024-01-29 02:46:45 -0400

    Added Comments
    Added a few comments to explain functionality. Should be relatively simple enough though.

--- src/data_processing/database.py
+++ src/data_processing/database.py
@@ -4,6 +4,10 @@ January 26, 2024,
 Simple code to connect to database, close connection so that you do not need to repeat/remember code every time
 
 Also, a function to create tables for file names and dates, that I am using with the scraping functionality
+
+Cian Bottomley-Mason
+January 28, 2024,
+Basic database-wide search functionality using Isaac's scraped filename system
 """
 
 
@@ -50,14 +54,21 @@ def create_file_name_tables(connection):
 			"""CREATE TABLE Local_file_names(file_name VARCHAR(255),file_date VARCHAR(255));""")
 
 
+# Search functionality: returns a list of items matching the search parameters from all lists
+# Keeps duplicate items at the moment, not sure if we should also include the publisher to distinguish dupes,
+# or if we should just remove them instead (keeping whichever one has the best access value, i.e. if one says "Y", delete the other?
 def search_database(connection, searchType, value):
 	results = []
 	cursor = connection.cursor()
+
+	# Grabs the names of all tables via the CRKN and local file name tables
 	listOfTables = connection.execute(
 		"""SELECT file_name FROM CRKN_file_names
 		UNION
 		SELECT file_name FROM Local_file_names;""").fetchall()
-	listOfTables = [row[0] for row in listOfTables]
+	listOfTables = [row[0] for row in listOfTables] # neat way to strip the apostrophes/parentheses from python's default formatting
+
+	# Searches for matching items through each table one by one and adds any matches to the list
 	for table in listOfTables:
 		if searchType == 'Title':
 			value = f'%{value}%'
@@ -69,6 +80,7 @@ def search_database(connection, searchType, value):
 	return results
 
 
+# individual search functions for each search type
 def search_by_title(connection, value):
 	return search_database(connection, "Title", value)
 
