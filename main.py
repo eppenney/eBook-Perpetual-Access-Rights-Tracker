c05e5c8d9983bf346493280d02043cbe9092023e
VipJatt07 <dgill7832@upei.ca>
2024-03-14 01:09:19 -0300

    Changes in the main files for the first time file opening and bar on the search display that when cell clicked should show the full length on bar above.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import sqlite3
 import sys
-from PyQt6.uic import loadUi
+
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication
-from src.user_interface.startScreen import startScreen
-from src.user_interface.settingsPage import settingsPage
-from src.utility.settings_manager import Settings # Import the settings manager
-from src.data_processing.database import connect_to_database, create_file_name_tables, close_database
+from PyQt6.QtCore import QThread, pyqtSignal, Qt
+from PyQt6.QtWidgets import QApplication, QProgressDialog
+
 from src.data_processing.Scraping import scrapeCRKN
+from src.data_processing.database import connect_to_database, create_file_name_tables, close_database
+from src.user_interface.settingsPage import settingsPage
+from src.user_interface.startScreen import startScreen
+from src.utility.settings_manager import Settings  # Import the settings manager
+
+
+class ScraperThread(QThread):
+    finished = pyqtSignal()
+
+    def run(self):
+        scrapeCRKN()  # Place your long-running task here
+        self.finished.emit()  # Emit signal when
+
+
+
+
 
 def main():
 
@@ -23,7 +36,19 @@ def main():
     app = QApplication(sys.argv)
     widget = QtWidgets.QStackedWidget()
 
-    #adding new code here please edit if it does not work
+    progressDialog = QProgressDialog("Scraping data, please wait...", None, 0, 0)
+    progressDialog.setCancelButton(None)  # Disable the Cancel button
+    progressDialog.setWindowModality(Qt.WindowModality.ApplicationModal)
+    progressDialog.show()
+
+    scraperThread = ScraperThread()
+    scraperThread.finished.connect(progressDialog.close)  # Close dialog when done
+    scraperThread.finished.connect(lambda: afterScraping(widget, settings_manager))  # Proceed after scraping
+    scraperThread.start()
+    sys.exit(app.exec())
+
+
+def afterScraping(widget, settings_manager):
     if settings_manager.is_first_launch():
         settings = settingsPage.get_instance(widget)
         widget.addWidget(settings)
@@ -32,13 +57,10 @@ def main():
         start = startScreen.get_instance(widget)
         widget.addWidget(start)
 
-
-    # start = startScreen.get_instance(widget)  # Pass the widget to startScreen
-    # widget.addWidget(start)
     widget.setMinimumHeight(800)
     widget.setMinimumWidth(1200)
     widget.show()
-    sys.exit(app.exec())
+
 
 
 if __name__ == "__main__":
