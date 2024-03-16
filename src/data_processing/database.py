aeefbcd422ec95e7fed49f2a9ad5ad00fcaf0fe6
Beakal <78448385+bbegashaw@users.noreply.github.com>
2024-03-15 22:27:10 -0300

    Merge remote-tracking branch 'origin/Baki_testing_branch' into Baki_testing_branch

--- src/data_processing/database.py
+++ src/data_processing/database.py
@@ -90,17 +90,16 @@ def create_file_name_tables(connection):
     # cursor object to interact with database
     cursor = connection.cursor()
 
-    # Only show if allow_CRKN is set to true
-    allow_crkn = settings_manager.get_setting('allow_CRKN')
-    if allow_crkn == "True":
-        list_of_tables = cursor.execute(
-            """SELECT name FROM sqlite_master WHERE type='table'
-            AND name='CRKN_file_names'; """).fetchall()
-        # If table doesn't exist, create new table for CRKN file info
-        if not list_of_tables:
-            print("Table does not exist, creating new one")
-            cursor.execute("CREATE TABLE CRKN_file_names(file_name VARCHAR(255), file_date VARCHAR(255));")
+    list_of_tables = cursor.execute(
+        """SELECT name FROM sqlite_master WHERE type='table'
+        AND name='CRKN_file_names'; """).fetchall()
+     # If table doesn't exist, create new table for CRKN file info
+    if not list_of_tables:
+        print("Table does not exist, creating new one")
+        cursor.execute("CREATE TABLE CRKN_file_names(file_name VARCHAR(255), file_date VARCHAR(255));")
 
+	# Empty list for next check
+	list_of_tables.clear()
     list_of_tables = cursor.execute(
         """SELECT name FROM sqlite_master WHERE type='table'
 		AND name='local_file_names'; """).fetchall()
